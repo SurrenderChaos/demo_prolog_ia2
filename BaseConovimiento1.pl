5331820aad4cf39971b4f8c6d28679ecc449e4ae
lucho1303 <benavides.lucho@hotmail.com>
2020-04-28 08:03:57 -0500

    adaptacion al proyecto

--- BaseConovimiento1.pl
+++ BaseConovimiento1.pl
@@ -3,20 +3,12 @@ Dominio: diagnostico medico. Trata los sintomas como una lista multi-elementos
 */
 /*Hice un comentario
 */
-conocimiento('sarampion',
-['el paciente esta cubierto de puntos', 'el paciente tiene temperatura alta',
-'el paciente tiene ojos rojos','el paciente tiene tos seca']).
-conocimiento('influenza',
-['el paciente tiene dolor en las articulaciones', 'el paciente tiene mucho
-estornudo','el paciente tiene dolor de cabeza']).
-conocimiento('malaria',
-['el paciente tiene temperatura alta','el paciente tiene dolor en las
-articulaciones', 'el paciente tiembla violentamente', 'el paciente tiene
-escalofrios']).
-conocimiento('gripe',
-['el paciente tiene cuerpo cortado', 'el paciente tiene dolor de cabeza', 'el
-paciente tiene temparatura alta']).
-conocimiento('tifoidea',
-['el paciente tiene falta de apetito', 'el paciente tiene temperatura alta', 'el
-paciente tiene dolor abdominal', 'el paciente tiene dolor de cabeza', 'el paciente
-tiene diarrea']).
+conocimiento('vendedor',
+['compra 20 pantallas cada mes', 'compra articulos con precios reducidos',
+'compra mas de 10 productos baratos a la semana','compra mas de 10 productos a la semana',
+'compra mas de un tipo de producto']).
+conocimiento('ingeniero',
+['compra 2 memorias ram por año', 'compra articulos con altas calificaciones en calidad','revisa productos de tecnologia con regularidad','compra al menos 3 libros de ingenieria al año','compra en su mayoria productos de tecnologia']).
+conocimiento('artista',
+['compra 2 lienzos por semana','compra menos de 3 lienzos por semana', 'compra menos de 10 pinturas por mes', 'compra menos de 5 libros de arte al año','compra articulos de artede calidad']).
+
